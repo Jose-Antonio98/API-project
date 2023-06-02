9d49c2650f19f755465e30e65b5b9f3c53283624
Jose-Antonio98 <joseantoniochavesjunior@gmail.com>
2023-06-02 17:56:53 -0300

    first commit

---
+++ src/main/java/edu/joseph/sed/SedApplication.java
@@ -0,0 +1,13 @@
+package edu.joseph.sed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SedApplication.class, args);
+	}
+
+}
